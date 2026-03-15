514334b32d208080860c86a96d2157e53a0efaed
Hassan El-Meligy <18198224+hassan210360@users.noreply.github.com>
2026-03-15 03:15:24 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 
-async function searchPolicies(){
+const pages=[
+"01_governance","02_investment","03_leasing","04_facility_management",
+"05_capex_projects","06_performance","07_risk_compliance","08_master_data",
+"09_ai_knowledge_hub","10_management_administration","11_finance_accounting",
+"12_treasury_cash","13_procurement_commercial","14_human_resources",
+"15_it_data_governance","16_sales_marketing","17_tenant_service",
+"18_legal_contracts","19_internal_control_audit","20_ethics_conduct",
+"21_business_continuity"
+]
+
+function runSearch(){
 let q=document.getElementById("search").value.toLowerCase()
-let res=document.getElementById("results")
-res.innerHTML=""
-if(q.length<2){return}
-let data=await fetch('data/policies.json').then(r=>r.json())
-data.forEach(p=>{
-if(p.title.toLowerCase().includes(q)){
+let results=document.getElementById("results")
+results.innerHTML=""
+pages.forEach(p=>{
+if(p.includes(q)){
 let div=document.createElement("div")
-div.innerHTML="<a href='"+p.url+"'>"+p.title+"</a>"
-res.appendChild(div)
+div.innerHTML="<a href='documents/"+p+".html'>"+p.replaceAll("_"," ")+"</a>"
+results.appendChild(div)
 }
 })
-}
-
-function toggleGroup(id){
-let el=document.getElementById(id)
-el.style.display = el.style.display==="none"?"block":"none"
 }
